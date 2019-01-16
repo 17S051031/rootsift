654c56ae062239b06c9ff2e3f70cafeec5b2e257
hudawei <526451080@qq.com>
2019-01-16 08:47:05 +0800

    Initial commit

---
+++ rootsift/driver.py
@@ -0,0 +1,25 @@
+# USAGE
+# python driver.py
+
+# import the necessary packages
+from rootsift import RootSIFT
+import cv2
+
+# load the image we are going to extract descriptors from and convert
+# it to grayscale
+image = cv2.imread("example.png")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# detect Difference of Gaussian keypoints in the image
+detector = cv2.FeatureDetector_create("SIFT")
+kps = detector.detect(gray)
+
+# extract normal SIFT descriptors
+extractor = cv2.DescriptorExtractor_create("SIFT")
+(kps, descs) = extractor.compute(gray, kps)
+print "SIFT: kps=%d, descriptors=%s " % (len(kps), descs.shape)
+
+# extract RootSIFT descriptors
+rs = RootSIFT()
+(kps, descs) = rs.compute(gray, kps)
+print "RootSIFT: kps=%d, descriptors=%s " % (len(kps), descs.shape)
